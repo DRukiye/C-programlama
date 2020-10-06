9720f0320612136349aeba58beec4f515a9671fc
Rukiye Demirci <71218414+DRukiye@users.noreply.github.com>
2020-10-06 19:01:46 +0300

    Update and rename Bir sayının toplam ve çarpımını bulma.txt to Belirlenen bir sayının toplamı ve faktöriyelini hesaplama.c

--- Belirlenen bir sayının toplamı ve faktöriyelini hesaplama.c
+++ Belirlenen bir sayının toplamı ve faktöriyelini hesaplama.c
@@ -10,15 +10,15 @@ int main(void)
 	{
 		toplam = toplam+i;			   
 	}
-    printf("%d\n",toplam);
+    	printf("%d\n",toplam);
     
-    int carpim=1;
-    for(i=1;i<=a;i++)
-    {
-    	carpim *= i;
- 	
+   	 int carpim=1;
+  	  for(i=1;i<=a;i++)
+    	{
+		carpim *= i;
+
 	}
-    printf("faktoriyel= %d",carpim);
+   	 printf("faktoriyel= %d",carpim);
     
    return 0;
 }
